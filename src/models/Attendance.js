9cb300dc4b75f9f1df08aeb18fba547cec0fb10f
Your Name <you@example.com>
2024-07-13 10:42:59 +0700

    .

--- src/models/Attendance.js
+++ src/models/Attendance.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const attendanceSchema = new Schema({
-  // eventId: { type: String, required: true, ref: 'Event' },
-  // lecturerId: { type: String, required: true, ref: 'Lecturer' },
   eventId: { type: String, required: true, unique: true },
   lecturers: { type: Array, required: true, },
- 
- 
 });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
