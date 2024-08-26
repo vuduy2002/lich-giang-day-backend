fd2cfc99c2ad942c8053cc1814c98c52747516ad
Your Name <you@example.com>
2024-08-26 17:23:56 +0700

    .

--- src/app.js
+++ src/app.js
@@ -13,32 +13,42 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// // Import routes
-// const attendanceRoutes = require('./routes/attendance');
-// const eventRoutes = require('./routes/event');
-// const eventTypeRoutes = require('./routes/eventType');
-// const lecturerRoutes = require('./routes/lecturer');
-// const resetPass = require('./routes/resetPass');
-// const locationRoutes = require('./routes/location');
+// Import routes
+const attendanceRoutes = require('./routes/attendance');
+const eventRoutes = require('./routes/event');
+const eventTypeRoutes = require('./routes/eventType');
+const lecturerRoutes = require('./routes/lecturer');
+const resetPass = require('./routes/resetPass');
+const locationRoutes = require('./routes/location');
 
-// // Use routes
-// app.use('/api/attendances', attendanceRoutes);
-// app.use('/api/events', eventRoutes);
-// app.use('/api/eventTypes', eventTypeRoutes);
-// app.use('/api/lecturers', lecturerRoutes);
-// app.use('/api/', resetPass);
-// app.use('/api/locations', locationRoutes);
+// Use routes
+app.use('/api/attendances', attendanceRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/eventTypes', eventTypeRoutes);
+app.use('/api/lecturers', lecturerRoutes);
+app.use('/api/', resetPass);
+app.use('/api/locations', locationRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log('MongoDB connected');
+//   app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+//     // dailyNotificationJob();
+//   });
+// }).catch(err => {
+//   console.error('Connection error', err.message);
+// });
+
+console.log('MongoDB connected');
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     // dailyNotificationJob();
   });
-}).catch(err => {
-  console.error('Connection error', err.message);
+
+  app.get('/', (req, res) => {
+    res.send('Hello, Heroku!');
 });
