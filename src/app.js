4cc0d66b9be8046aaca0cf1784d5b65ae81a0be5
Your Name <you@example.com>
2024-08-26 17:14:11 +0700

    .

--- src/app.js
+++ src/app.js
@@ -1,59 +1,44 @@
-// // src/app.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const dailyNotificationJob = require('./controllers/notificationController');
-
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT ;
-
-// app.use(cors());
-// app.use(express.json());
-
-// // Import routes
-// const attendanceRoutes = require('./routes/attendance');
-// const eventRoutes = require('./routes/event');
-// const eventTypeRoutes = require('./routes/eventType');
-// const lecturerRoutes = require('./routes/lecturer');
-// const resetPass = require('./routes/resetPass');
-// const locationRoutes = require('./routes/location');
-
-// // Use routes
-// app.use('/api/attendances', attendanceRoutes);
-// app.use('/api/events', eventRoutes);
-// app.use('/api/eventTypes', eventTypeRoutes);
-// app.use('/api/lecturers', lecturerRoutes);
-// app.use('/api/', resetPass);
-// app.use('/api/locations', locationRoutes);
-
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => {
-//   console.log('MongoDB connected');
-//   app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-//     dailyNotificationJob();
-//   });
-// }).catch(err => {
-//   console.error('Connection error', err.message);
-// });
-
-// index.js
+// src/app.js
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const dailyNotificationJob = require('./controllers/notificationController');
+
+dotenv.config();
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 4000;
 
-// Định nghĩa một route đơn giản
-app.get('/', (req, res) => {
-  res.send('duy dep trai');
-});
+app.use(cors());
+app.use(express.json());
 
-// Lắng nghe trên port 3000
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Import routes
+const attendanceRoutes = require('./routes/attendance');
+const eventRoutes = require('./routes/event');
+const eventTypeRoutes = require('./routes/eventType');
+const lecturerRoutes = require('./routes/lecturer');
+const resetPass = require('./routes/resetPass');
+const locationRoutes = require('./routes/location');
+
+// Use routes
+app.use('/api/attendances', attendanceRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/eventTypes', eventTypeRoutes);
+app.use('/api/lecturers', lecturerRoutes);
+app.use('/api/', resetPass);
+app.use('/api/locations', locationRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    // dailyNotificationJob();
+  });
+}).catch(err => {
+  console.error('Connection error', err.message);
 });
