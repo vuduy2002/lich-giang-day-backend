fddb4bc0f057e61c5b76bc833bf7705ec9cc9276
Your Name <you@example.com>
2024-08-27 16:20:17 +0700

    /

--- src/app.js
+++ src/app.js
@@ -30,15 +30,23 @@ app.use('/api/', resetPass);
 app.use('/api/locations', locationRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    dailyNotificationJob();
-  });
-}).catch(err => {
-  console.error('Connection error', err.message);
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log('MongoDB connected');
+//   app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+//     dailyNotificationJob();
+//   });
+// }).catch(err => {
+//   console.error('Connection error', err.message);
+// });
+app.get('/', (req, res) => {
+  res.send('Hello, World with duy!');
 });
+
+
+console.log('MongoDB connected');
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);})
